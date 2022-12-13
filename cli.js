05492922e9d74f4c803319c58709c51d75539edc
Liron Toledo <ldoggy20@gmail.com>
2022-12-13 09:28:27 +0200

    Swapped rows and interactions in header

--- cli.js
+++ cli.js
@@ -181,7 +181,7 @@ function pfizerFormat(objects, save) {
     }
     
     // Add header and trailer
-    let headerTrailer = `VAT|${objects.length}|${pfizerFormat.length}|${today}|${time}|`
+    let headerTrailer = `VAT|${pfizerFormat.length}|${objects.length}|${today}|${time}|`
     pfizerFormat.unshift(headerTrailer)
     pfizerFormat.push(headerTrailer)
     
